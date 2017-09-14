8a83f3ec2227aa7e739352beace0b140f05b220b
jairo <jairog12@outlook.com>
2017-09-13 23:05:24 -0500

    add specs to list component

--- src/app/components/listado/listado.component.spec.ts
+++ src/app/components/listado/listado.component.spec.ts
@@ -22,4 +22,20 @@ describe('ListadoComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+  
+  it("should be show a list of a last 10 items", () => {
+
+  })
+
+  it("should be show a title, description and status (Done, in process, Todo)", ()=>{
+
+  })
+
+  it("It should show in green those that are already fulfilled, in yellow those that are to be realized, and in orange those that are in process.", ()=>{
+    // it may be necessary to distribute them in other specifications :D
+  })
+
+  it("Each time I add a new task it is placed at the top of the list.", ()=>{
+
+  })
 });
